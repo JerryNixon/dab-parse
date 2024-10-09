9480aaa8cc54058674f26e7bcb3d141bc91972e2
Jerry Nixon <1749983+JerryNixon@users.noreply.github.com>
2024-10-08 18:24:20 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -88,7 +88,14 @@ internal class Program
 			{
 				0 => '0', // No entities
 				1 => '1', // One entity
-				>= 2 and <= 9 => '0', // 2-9 entities
+				2 => '2',
+				3 => '3',
+				4 => '4',
+				5 => '5',
+				6 => '6',
+				7 => '7',
+				8 => '8',
+				9 => '9',
 				_ => 'A' // More than 9
 			};
 
